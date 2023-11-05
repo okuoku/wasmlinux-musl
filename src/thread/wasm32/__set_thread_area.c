52cf4694f70a43dab1e2d71be6776dde3150b8d5
Yuki Okumura <mjt@cltn.org>
2023-11-06 00:52:08 +0900

    wasm32: TLS R/W ops

--- src/thread/wasm32/__set_thread_area.c
+++ src/thread/wasm32/__set_thread_area.c
@@ -1,8 +1,10 @@
+#include <stdint.h>
+
 extern int32_t
 __attribute__((import_module("env"), import_name("wasmlinux_tlsrw32")))
 wasmlinux_tlsrw32(uint32_t op, uint32_t val);
 
-static inline uintptr_t 
-__get_tp(){
-    return wasmlinux_tlsrw32(1, 0);
+int 
+__set_thread_area(void *p) {
+    return wasmlinux_tlsrw32(0, (uint32_t)p);
 }
