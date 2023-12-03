5517c7cc222403869d52f8f6a84acfd832b9f6ce
okuoku <mjt@cltn.org>
2023-12-03 17:19:06 +0900

    wasm: Implement `main` trampoline
    (Tentative) Implement `main` trampoline logic that'd allow
    2-ary and 3-ary main function.

--- crt/crt1.c
+++ crt/crt1.c
@@ -5,7 +5,19 @@
 
 #include "crt_arch.h"
 
+#ifndef __wasm__
 int main();
+#else
+int __main_argc_argv(int ac, char** av);
+weak int main(int ac, char** av, char** envp){
+	(void) envp;
+	return __main_argc_argv(ac, av);
+}
+static int wasmlinux_libc_main(int ac, char** av, char** envp){
+	main(ac, av, envp);
+}
+#endif
+
 weak void _init();
 weak void _fini();
 int __libc_start_main(int (*)(), int, char **,
@@ -15,5 +27,10 @@ void _start_c(long *p)
 {
 	int argc = p[0];
 	char **argv = (void *)(p+1);
+#ifndef __wasm__
 	__libc_start_main(main, argc, argv, _init, _fini, 0);
+#else
+	__libc_start_main(wasmlinux_libc_main, argc, argv, _init, _fini, 0);
+#endif
 }
+
